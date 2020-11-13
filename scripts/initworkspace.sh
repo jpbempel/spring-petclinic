4133aee57b3e95f8785d3605744f4dbe446f13bd
Jean-Philippe Bempel <jean-philippe@bempel.fr>
2020-11-13 16:18:27 +0100

    Update iniworkspace.sh

--- scripts/initworkspace.sh
+++ scripts/initworkspace.sh
@@ -6,20 +6,19 @@ else
   BASEDIR=$1
 fi
 echo "Base directory: $BASEDIR"
-if [ ! -d $BASEDIR/jdk/zulu8.46.0.19-ca-jdk8.0.252-linux_x64 ];
+if [ ! -d $BASEDIR/jdk ];
 then
   echo "Downloading JDKs..."
   pushd .
   cd $BASEDIR
   mkdir jdk
   cd jdk
-  curl -OL https://cdn.azul.com/zulu/bin/zulu8.46.0.19-ca-jdk8.0.252-linux_x64.tar.gz
-  tar xzvf zulu8.46.0.19-ca-jdk8.0.252-linux_x64.tar.gz
-  curl -OL https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.7%2B10/OpenJDK11U-jdk_x64_linux_hotspot_11.0.7_10.tar.gz
-  tar xzvf OpenJDK11U-jdk_x64_linux_hotspot_11.0.7_10.tar.gz
-  curl -OL https://builds.shipilev.net/openjdk-jdk8-dev/openjdk-jdk8-dev-latest-linux-x86_64-release.tar.xz
-  tar xJvf openjdk-jdk8-dev-latest-linux-x86_64-release.tar.xz
-  mv j2sdk-image jdk8dev
+  curl -OL https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u275-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz
+  tar xzvf OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz
+  curl -OL https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.9%2B11.1/OpenJDK11U-jdk_x64_linux_hotspot_11.0.9_11.tar.gz
+  tar xzvf OpenJDK11U-jdk_x64_linux_hotspot_11.0.9_11.tar.gz
+  curl -OL https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.1%2B9/OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz
+  tar xzvf OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz
   popd
 fi
 if [ ! -d $BASEDIR/ap ];
@@ -32,5 +31,5 @@ then
 fi
 echo "Building spring-petclinic"
 cd $BASEDIR/spring-petclinic
-export JAVA_HOME=$BASEDIR/jdk/zulu8.46.0.19-ca-jdk8.0.252-linux_x64
-./mvnw package -DskipTests=true
+export JAVA_HOME=$BASEDIR/jdk/jdk8u275-b01
+./mvnw spring-javaformat:apply package -DskipTests=true
